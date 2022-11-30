976fb4c38b1ca4b8d4fee3cd701564a2cc763305
José Romero <64669439+joseph78722@users.noreply.github.com>
2022-11-30 11:45:24 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  const [pacientes, setPacientes] = useState(
+    () => JSON.parse(localStorage.getItem("pacientes")) || []
+  );
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesActualizados);
+  };
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          paciente={paciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+      <div className=" font-bold mb-3 text-gray-700 bg-gray-200">
+        <p>Aplicación creada por José Romero</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
